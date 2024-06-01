129de710cbc5f755164d46c9a4800b8db56d4f1e
Thomas Martinez <tmartinez114@gmail.com>
2024-06-01 12:19:18 -0400

    added style for not avaialble

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -3,13 +3,21 @@ import React from "react"
 // using props from Rooms obj
 const Room = ({ id, name, beds, price, available }) => {
   return (
-    <div className="room">
-      <p>{name}</p>
-      <p>Beds: {beds}</p>
-      <p>Rate: ${price}/night</p>
-      <b>{available ? "Available" : "Not Available"}</b>
+    <>
       <hr></hr>
-    </div>
+      <div className="room">
+        <p>{name}</p>
+        <p>Beds: {beds}</p>
+        <p>Rate: ${price}/night</p>
+        <b>
+          {available ? (
+            <button>Book Now</button>
+          ) : (
+            <p style={{ color: "red" }}>Not Available</p>
+          )}
+        </b>
+      </div>
+    </>
   )
 }
 
