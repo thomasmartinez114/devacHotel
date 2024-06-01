322671a61e496ad944256613abed8a12ac92d8c8
Thomas Martinez <tmartinez114@gmail.com>
2024-06-01 10:30:53 -0400

    showing room on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css"
 
 function App() {
-  return <h1>Hello World</h1>
+  // hotel obj with rooms
+  let hotel = {
+    rooms: {
+      single: {
+        beds: 1,
+        price: 100,
+      },
+      double: {
+        beds: 2,
+        price: 200,
+      },
+      suite: {
+        beds: 1,
+        price: 500,
+      },
+    },
+  }
+
+  return (
+    <div className="App">
+      <h1>Hotel Rooms</h1>
+      <ul>
+        {Object.keys(hotel.rooms).map((room) => (
+          <li key={room}>
+            <h2>{room.charAt(0).toUpperCase() + room.slice(1)} Room</h2>
+            <p>Beds: {hotel.rooms[room].beds}</p>
+            <p>Price: ${hotel.rooms[room].price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
