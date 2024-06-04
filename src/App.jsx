9d01b6d62aefad3abcf9e192d630fe090fc520b9
Thomas Martinez <tmartinez114@gmail.com>
2024-06-03 22:34:08 -0400

    moved header into it own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css"
 import DaysSelector from "./components/DaysSelector"
+import Header from "./components/Header"
 import RoomsList from "./components/RoomsList"
 
 const App = () => {
   return (
     <div>
-      <h1>Devac Hotel</h1>
+      <Header />
       <DaysSelector />
       <RoomsList />
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Header = () => {
+  return <h1>Devac Hotel</h1>
+}
+
+export default Header
